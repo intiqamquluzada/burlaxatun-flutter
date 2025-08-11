02b87fd7cec628fd19392667b253261aa4876991
nihadzzz <nihadosmanov21@gmail.com>
2025-08-11 17:22:51 +0400

    - add indicator updated

--- lib/ui/screens/main/views/home_page/my_healing_page/indicator_data_screen/widgets/add_new_indicator_dialog.dart
+++ lib/ui/screens/main/views/home_page/my_healing_page/indicator_data_screen/widgets/add_new_indicator_dialog.dart
@@ -1,18 +1,18 @@
-import 'package:burla_xatun/cubits/indicator/indicator_cubit.dart';
-import 'package:burla_xatun/cubits/user_data/user_data_cubit.dart';
-import 'package:burla_xatun/ui/screens/main/views/home_page/my_healing_page/indicator_data_screen/widgets/date_or_time_box.dart';
-import 'package:burla_xatun/ui/screens/main/views/home_page/my_healing_page/indicator_data_screen/widgets/pick_indicator_date_widget.dart';
-import 'package:burla_xatun/utils/app/app_snackbars.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:go_router/go_router.dart';
 import 'package:intl/intl.dart';
 
+import '../../../../../../../../cubits/indicator/indicator_cubit.dart';
+import '../../../../../../../../cubits/user_data/user_data_cubit.dart';
+import '../../../../../../../../utils/app/app_snackbars.dart';
 import '../../../../../../../../utils/constants/color_constants.dart';
 import '../../../../../../../../utils/extensions/num_extensions.dart';
 import '../../../../../../../widgets/global_text.dart';
 import 'add_indicator_input.dart';
+import 'date_or_time_box.dart';
+import 'pick_indicator_date_widget.dart';
 
 class AddNewIndicatorDialog extends StatefulWidget {
   const AddNewIndicatorDialog({
@@ -41,183 +41,189 @@ class _AddNewIndicatorDialogState extends State<AddNewIndicatorDialog> {
     final IndicatorCubit indicatorCubit = context.read<IndicatorCubit>();
 
     return Center(
-      child: SizedBox(
-        width: MediaQuery.of(context).size.width * 0.9,
-        height: 480,
-        child: Material(
-          type: MaterialType.transparency,
-          child: DecoratedBox(
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.all(Radius.circular(8)),
-            ),
-            child: Column(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.only(top: 22, right: 32),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      GestureDetector(
-                        child: SvgPicture.asset('assets/icons/close_icon.svg'),
-                        onTap: () {
-                          Navigator.pop(context);
-                        },
-                      )
-                    ],
-                  ),
-                ),
-                GlobalText(
-                  //text: 'New indicator',
-                  text: 'Yeni göstərici',
-                  fontSize: 20,
-                  fontWeight: FontWeight.w600,
-                  color: Colors.black,
-                ),
-                30.h,
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 24),
-                  child: Column(
-                    spacing: 20,
-                    children: [
-                      AddIndicatorInput(
-                        inputName: 'Bədən çəkisi',
-                        hintText: 'Kq',
-                        controller: _indicatorController,
-                      ),
-                      ValueListenableBuilder(
-                        valueListenable: dateValue,
-                        builder: (context, value, child) {
-                          final selectedDate =
-                              DateFormat('yyyy-MM-dd').format(value);
-                          return DateOrTimeBox(
-                            inputName: 'Tarix',
-                            hintText: selectedDate,
-                            onTap: () {
-                              showModalBottomSheet(
-                                context: context,
-                                builder: (context) {
-                                  return PickIndicatorDateWidget(
-                                    dateValue: dateValue,
-                                  );
-                                },
-                              );
-                            },
-                          );
-                        },
-                      ),
-                      ValueListenableBuilder(
-                        valueListenable: timeValue,
-                        builder: (_, value, child) {
-                          final hintTime = DateFormat('HH:mm').format(value);
-                          return DateOrTimeBox(
-                            hintText: hintTime,
-                            inputName: 'Vaxt',
-                            onTap: () {
-                              showModalBottomSheet(
-                                context: context,
-                                builder: (context) {
-                                  return PickIndicatorDateWidget(
-                                    isTimeSelecting: true,
-                                    timeValue: timeValue,
-                                  );
-                                },
-                              );
-                            },
-                          );
-                        },
-                      ),
-                    ],
-                  ),
-                ),
-                20.h,
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    SizedBox(
-                      height: 44,
-                      width: 140,
-                      child: ElevatedButton(
-                        style: ButtonStyle(
-                          shadowColor:
-                              const WidgetStatePropertyAll(Colors.transparent),
-                          backgroundColor:
-                              WidgetStatePropertyAll(Color(0xffECECEC)),
-                        ),
-                        onPressed: () {
-                          Navigator.pop(context);
-                        },
-                        child: GlobalText(
-                          text: 'İmtina Et',
-                          fontSize: 14,
-                          height: 1.1,
-                          fontWeight: FontWeight.w500,
-                          color: Colors.grey,
-                        ),
-                      ),
-                    ),
-                    SizedBox(width: 22),
-                    SizedBox(
-                      height: 44,
-                      width: 140,
-                      child: ElevatedButton(
-                        style: ButtonStyle(
-                          shadowColor:
-                              const WidgetStatePropertyAll(Colors.transparent),
-                          backgroundColor:
-                              WidgetStatePropertyAll(Color(0xffFFD3E2)),
-                        ),
-                        onPressed: () {
-                          final selectedDate =
-                              DateFormat('yyyy-MM-dd').format(dateValue.value);
-                          final selectedTime =
-                              DateFormat('HH:mm').format(timeValue.value);
-                          indicatorCubit.addIndicator(
-                            indicatorName: widget.indicatorName,
-                            indicator: _indicatorController.text.trim(),
-                            date: selectedDate,
-                            time: selectedTime,
-                            babyId: context
-                                    .read<UserDataCubit>()
-                                    .currentBabyNotifier
-                                    .value
-                                    ?.id ??
-                                -1,
-                          );
-                        },
-                        child: BlocConsumer<IndicatorCubit, IndicatorState>(
-                          listener: (context, state) {
-                            if (state.indicatorStatus ==
-                                IndicatorStatus.error) {
-                              context.pop();
-                              AppSnackbars.error(context,
-                                  'Göstərici əlavə edərkən xəta baş verdi');
-                            } else if (state.indicatorStatus ==
-                                IndicatorStatus.success) {
-                              context.pop();
-                              AppSnackbars.success(
-                                  context, 'Göstərici uğurla əlavə olundu');
-                            }
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: SizedBox(
+          width: MediaQuery.of(context).size.height * 0.5,
+          height: 480,
+          child: Material(
+            type: MaterialType.transparency,
+            child: DecoratedBox(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.all(Radius.circular(12)),
+              ),
+              child: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(top: 22, right: 32),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        GestureDetector(
+                          child:
+                              SvgPicture.asset('assets/icons/close_icon.svg'),
+                          onTap: () {
+                            Navigator.pop(context);
                           },
-                          builder: (context, state) {
-                            if (state.indicatorStatus ==
-                                IndicatorStatus.loading) {
-                              return CircularProgressIndicator.adaptive();
-                            }
-                            return GlobalText(
-                              text: 'Yadda saxla',
-                              fontSize: 14,
-                              height: 1.1,
-                              fontWeight: FontWeight.w500,
-                              color: ColorConstants.primaryRedColor,
+                        )
+                      ],
+                    ),
+                  ),
+                  GlobalText(
+                    //text: 'New indicator',
+                    text: 'Yeni göstərici',
+                    fontSize: 20,
+                    fontWeight: FontWeight.w600,
+                    color: Colors.black,
+                  ),
+                  30.h,
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 24),
+                    child: Column(
+                      spacing: 20,
+                      children: [
+                        AddIndicatorInput(
+                          inputName: 'Göstərici',
+                          hintText: 'Göstərici',
+                          controller: _indicatorController,
+                        ),
+                        ValueListenableBuilder(
+                          valueListenable: dateValue,
+                          builder: (context, value, child) {
+                            final selectedDate =
+                                DateFormat('yyyy-MM-dd').format(value);
+                            return DateOrTimeBox(
+                              inputName: 'Tarix',
+                              hintText: selectedDate,
+                              onTap: () {
+                                showModalBottomSheet(
+                                  context: context,
+                                  builder: (context) {
+                                    return PickIndicatorDateWidget(
+                                      dateValue: dateValue,
+                                    );
+                                  },
+                                );
+                              },
                             );
                           },
                         ),
-                      ),
+                        ValueListenableBuilder(
+                          valueListenable: timeValue,
+                          builder: (_, value, child) {
+                            final hintTime = DateFormat('HH:mm').format(value);
+                            return DateOrTimeBox(
+                              hintText: hintTime,
+                              inputName: 'Vaxt',
+                              onTap: () {
+                                showModalBottomSheet(
+                                  context: context,
+                                  builder: (context) {
+                                    return PickIndicatorDateWidget(
+                                      isTimeSelecting: true,
+                                      timeValue: timeValue,
+                                    );
+                                  },
+                                );
+                              },
+                            );
+                          },
+                        ),
+                      ],
                     ),
-                  ],
-                ),
-              ],
+                  ),
+                  20.h,
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      SizedBox(
+                        height: 44,
+                        width: 140,
+                        child: ElevatedButton(
+                          style: ButtonStyle(
+                            shadowColor: const WidgetStatePropertyAll(
+                                Colors.transparent),
+                            backgroundColor:
+                                WidgetStatePropertyAll(Color(0xffECECEC)),
+                          ),
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
+                          child: GlobalText(
+                            textAlign: TextAlign.center,
+                            text: 'İmtina Et',
+                            fontSize: 14,
+                            height: 1.1,
+                            fontWeight: FontWeight.w500,
+                            color: Colors.grey,
+                          ),
+                        ),
+                      ),
+                      SizedBox(width: 22),
+                      SizedBox(
+                        height: 44,
+                        width: 140,
+                        child: ElevatedButton(
+                          style: ButtonStyle(
+                            shadowColor: const WidgetStatePropertyAll(
+                                Colors.transparent),
+                            backgroundColor:
+                                WidgetStatePropertyAll(Color(0xffFFD3E2)),
+                          ),
+                          onPressed: () {
+                            final selectedDate = DateFormat('yyyy-MM-dd')
+                                .format(dateValue.value);
+                            final selectedTime =
+                                DateFormat('HH:mm').format(timeValue.value);
+                            indicatorCubit.addIndicator(
+                              indicatorName: widget.indicatorName,
+                              indicator: _indicatorController.text.trim(),
+                              date: selectedDate,
+                              time: selectedTime,
+                              babyId: context
+                                      .read<UserDataCubit>()
+                                      .currentBabyNotifier
+                                      .value
+                                      ?.id ??
+                                  -1,
+                            );
+                          },
+                          child: BlocConsumer<IndicatorCubit, IndicatorState>(
+                            listener: (context, state) {
+                              if (state.indicatorStatus ==
+                                  IndicatorStatus.error) {
+                                context.pop();
+                                AppSnackbars.error(context,
+                                    'Göstərici əlavə edərkən xəta baş verdi');
+                              } else if (state.indicatorStatus ==
+                                  IndicatorStatus.success) {
+                                context.pop();
+                                AppSnackbars.success(
+                                    context, 'Göstərici uğurla əlavə olundu');
+                              }
+                            },
+                            builder: (context, state) {
+                              if (state.indicatorStatus ==
+                                  IndicatorStatus.loading) {
+                                return CircularProgressIndicator.adaptive();
+                              }
+                              return GlobalText(
+                                textAlign: TextAlign.center,
+                                text: 'Yadda saxla',
+                                fontSize: 14,
+                                height: 1.1,
+                                fontWeight: FontWeight.w500,
+                                color: ColorConstants.primaryRedColor,
+                              );
+                            },
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
             ),
           ),
         ),
