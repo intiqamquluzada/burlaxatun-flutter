168ca51376aedd99e8b51458eeb2ff919b0df5f1
NijatNaghiyev <nicat614@icloud.com>
2025-10-13 21:00:22 +0400

    Update Gradle and Flutter dependencies, improve project configuration
    - Updated Gradle wrapper to version 8.12 in gradle-wrapper.properties
    - Upgraded Android Gradle plugin to version 8.7.0 and Kotlin plugin to version 2.1.0 in settings.gradle
    - Removed unused dependencies from Podfile.lock and updated existing ones
    - Modified project.pbxproj to reflect changes in framework references and build phases
    - Updated Flutter-Generated.xcconfig with correct paths and build version
    - Adjusted flutter_export_environment.sh to match new Flutter version and paths
    - Updated pubspec.lock with new package versions and checksums
    - Changed version in pubspec.yaml to 2.0.5+23

--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\src\flutter"
-export "FLUTTER_APPLICATION_PATH=D:\burla_xatun\burla_xatun"
+export "FLUTTER_ROOT=/Users/nijatnaghiyev/development/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/nijatnaghiyev/Documents/AA_Solutions/burlaxatun-flutter"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
-export "FLUTTER_BUILD_NAME=2.0.2"
-export "FLUTTER_BUILD_NUMBER=21"
+export "FLUTTER_BUILD_NAME=2.0.5"
+export "FLUTTER_BUILD_NUMBER=23"
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=true"
 export "TREE_SHAKE_ICONS=false"
