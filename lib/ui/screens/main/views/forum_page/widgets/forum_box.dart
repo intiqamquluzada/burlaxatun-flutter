a88f84ad42a59ee05465fa765e2e8fd3c762a780
nihadzzz <nihadosmanov21@gmail.com>
2025-11-07 16:44:59 +0400

    - more icon removed

--- lib/ui/screens/main/views/forum_page/widgets/forum_box.dart
+++ lib/ui/screens/main/views/forum_page/widgets/forum_box.dart
@@ -3,7 +3,6 @@ import 'dart:developer';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 
-import '../../../../../../utils/extensions/context_extensions.dart';
 import '../../../../../../utils/extensions/num_extensions.dart';
 import '../../../../../widgets/global_text.dart';
 import '../../../../../widgets/report_or_block_forum.dart';
@@ -55,19 +54,22 @@ class ForumBox extends StatelessWidget {
                       color: Colors.black,
                     ),
                   ),
-                  Row(
-                    children: [
-                      GlobalText(
-                        text: 'Daha ətraflı',
-                        fontSize: 10,
-                        fontWeight: FontWeight.w500,
-                        color: Color(0xff97989D),
-                      ),
-                      SizedBox(width: 7),
-                      SvgPicture.asset(
-                        'assets/icons/medicine_arrow_right_icon.svg',
-                      ),
-                    ],
+                  Visibility(
+                    visible: !isCommentPage,
+                    child: Row(
+                      children: [
+                        GlobalText(
+                          text: 'Daha ətraflı',
+                          fontSize: 10,
+                          fontWeight: FontWeight.w500,
+                          color: Color(0xff97989D),
+                        ),
+                        SizedBox(width: 7),
+                        SvgPicture.asset(
+                          'assets/icons/medicine_arrow_right_icon.svg',
+                        ),
+                      ],
+                    ),
                   ),
                 ],
               ),
